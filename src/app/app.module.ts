76e703619f2cb4c005556afbf153ca0bd50dbdd2
Julián De León <julian_deleon@outlook.com>
2020-07-29 10:06:37 -0300

    Construccion responsive movil - Before navbar search mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 
+// Layout
+import { LayoutModule } from '@angular/cdk/layout';
+
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/home/header/header.component';
 import { SearchComponent } from './components/home/search/search.component';
+import { LocationComponent } from './components/home/search/location/location.component';
+import { PropiedadComponent } from './components/home/search/propiedad/propiedad.component';
+import { HuespedesComponent } from './components/home/search/huespedes/huespedes.component';
 
 // Forms
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 // Angular Material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { LocationComponent } from './components/home/search/location/location.component';
-import { PropiedadComponent } from './components/home/search/propiedad/propiedad.component';
-import { HuespedesComponent } from './components/home/search/huespedes/huespedes.component';
+import { NavbarMobileComponent } from './components/shared/navbar-mobile/navbar-mobile.component';
+
 
 @NgModule({
   declarations: [
@@ -31,6 +39,7 @@ import { HuespedesComponent } from './components/home/search/huespedes/huespedes
     LocationComponent,
     PropiedadComponent,
     HuespedesComponent,
+    NavbarMobileComponent,
   ],
   imports: [
     BrowserModule,
@@ -39,6 +48,7 @@ import { HuespedesComponent } from './components/home/search/huespedes/huespedes
     FormsModule,
     ReactiveFormsModule,
     MaterialModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  
+  navbar:boolean = true;
+
+  // BreakpointObserver se encargará de observar el tamaño de la pantalla en todo momento y evaluar los cambios que se producen. Esto me permitirá poder mostrar/ocultar elementos en base al tamaño de pantalla. 
+  constructor(private _observer: BreakpointObserver) {
+    this._observer.observe('(min-width: 700px)').subscribe(result => {
+      console.log(result);
+      this.navbar = !this.navbar;
+  });
+  }
 
   ngOnInit(): void {}
 }
